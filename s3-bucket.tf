bd4519caa20fc7745ebcc1b4c09058c648eb5222
Nathan New <nathanhnew@gmail.com>
2021-10-29 23:01:25 +0000

    [fix] fix versioning with module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,12 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
+module "s3-bucket" {
+  source  = "app.terraform.io/test-org-nnew/s3-bucket/aws"
+  
+  version = "2.2.0"
+  # insert required variables here
   bucket_prefix = "nathan-new"
   acl    = "private"
 
   versioning = {
     enabled = true
   }
-
 }
